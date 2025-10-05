b56743c34e12ff0b3aeb812ca9a93868ba0a40ff
Anne-Sophie Marchet <ton.email@example.com>
2025-10-05 17:47:44 +0200

    CLEVW-00000 refacto logger

--- src/main/aventurier/Map.java
+++ src/main/aventurier/Map.java
@@ -4,38 +4,47 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.List;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 
 
 
 public class Map {
-    private char[][] map;
+
     private static final Logger LOGGER = Logger.getLogger(Map.class.getName());
+    private final char[][] gameField;
 
     public Map(String mapName) throws IOException {
         List<String> lines = Files.readAllLines(Paths.get(mapName));
-        map = new char[lines.size()][];
+        gameField = new char[lines.size()][];
         for (int i = 0; i < lines.size(); i++) {
-            map[i] = lines.get(i).toCharArray();
+            gameField[i] = lines.get(i).toCharArray();
         }
-        LOGGER.info("Carte lue : " + mapName);
+        LOGGER.log(Level.INFO, "Carte lue : {0}", new Object[]{mapName});
     }
     public boolean isValidPosition(int x, int y) {
-        return x >= 0 && x < map[0].length && y >= 0 && y < map.length && map[y][x] != '#';
+        return x >= 0 && x < gameField[0].length && y >= 0 && y < gameField.length && gameField[y][x] != '#';
     }
 
-    public void displayMap(Hero hero) {
-        System.out.println("=======CARTE=======\n");
-        for (int y = 0; y < map.length; y++) {
-            for (int x = 0; x < map[y].length; x++) {
+    private String getMapAsString(Hero hero) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("=======CARTE=======\n");
+        for (int y = 0; y < gameField.length; y++) {
+            for (int x = 0; x < gameField[y].length; x++) {
                 if (hero != null && hero.getX() == x && hero.getY() == y) {
-                    System.out.print('H');
+                    sb.append('H');
                 } else {
-                    System.out.print(map[y][x]);
+                    sb.append(gameField[y][x]);
                 }
             }
-            System.out.println();
+            sb.append('\n');
         }
-        System.out.println("===================\n");
+        sb.append("===================\n");
+        return sb.toString();
+    }
+
+
+    public void displayMap(Hero hero) {
+        LOGGER.info(() -> getMapAsString(hero));
     }
 }
